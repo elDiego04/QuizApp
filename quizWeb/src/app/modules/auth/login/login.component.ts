bd511e481306bee6afd22582474103b7770d38b3
Diego Salazar <diego.salazarc4@gmail.com>
2025-02-10 15:14:47 -0500

    Update login feature

--- quizWeb/src/app/modules/auth/login/login.component.ts
+++ quizWeb/src/app/modules/auth/login/login.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  constructor(private fb: FormBuilder,
+    private authService: AuthService,
+    private message: NzMessageService,
+    private router: Router
+  ) {}
+  
+  validateForm!: FormGroup;
 
+  ngOnInit(){
+    this.validateForm = this.fb.group({
+      email: [null, Validators.required],
+      password: [null, Validators.required],
+    })
+  }
 }
