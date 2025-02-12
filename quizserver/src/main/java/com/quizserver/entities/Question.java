e916d812ecfcdce0f60667369873c7e46b5a29cd
Diego Salazar <diego.salazarc4@gmail.com>
2025-02-12 16:13:31 -0500

    Create Question entity and DTO and Update database relationship between Test and Question

---
+++ quizserver/src/main/java/com/quizserver/entities/Question.java
@@ -0,0 +1,24 @@
+package com.quizserver.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String questionText;
+    private String optionA;
+    private String optionB;
+    private String optionC;
+    private String optionD;
+
+    private String correctOption;
+
+    @ManyToOne
+    @JoinColumn(name = "test_id")
+    private Test test;
+}
