a96720b15c072b7f887f890cf30e69253c15cd52
Diego Salazar <diego.salazarc4@gmail.com>
2025-02-12 15:35:05 -0500

    Add admin services

--- quizWeb/src/app/modules/admin/components/create-test/create-test.component.ts
+++ quizWeb/src/app/modules/admin/components/create-test/create-test.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../../shared/shared.module';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AdminService } from '../../services/admin.service';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-test',
@@ -10,7 +13,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class CreateTestComponent {
 
-  constructor(private fb: FormBuilder) {} 
+  constructor(private fb: FormBuilder,
+    private deviceService: AdminService,
+    private notification: NzNotificationService,
+    private router: Router
+  ) {} 
     
   testForm!: FormGroup;
 
@@ -20,5 +27,26 @@ export class CreateTestComponent {
       description: [null, Validators.required],
       time: [null, Validators.required]
     })
+  }
+
+  submitForm(){
+    if (this.testForm.valid) {
+      this.deviceService.createTest(this.testForm.value).subscribe(res => {
+        this.notification
+        .success(
+          'SUCCESS',
+          'Test Created Successfully.',
+          {nzDuration: 5000}
+        );
+        this.router.navigateByUrl('/admin/dashboard');
+      }, error => {
+        this.notification
+        .error(
+          'Error',
+          `${error.error}`,
+          { nzDuration: 5000}
+        );
+      })
+    }
   }
 }
