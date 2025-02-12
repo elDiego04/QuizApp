6e2ac0576ec35efaa097c5e18042d85533aa96d3
Diego Salazar <diego.salazarc4@gmail.com>
2025-02-11 23:25:40 -0500

    Add endpoint test and test the endpoint

--- quizserver/src/main/java/com/quizserver/controller/TestController.java
+++ quizserver/src/main/java/com/quizserver/controller/TestController.java
@@ -1,14 +1,26 @@
 package com.quizserver.controller;
 
+import com.quizserver.dto.TestDTO;
 import com.quizserver.service.test.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/test")
+@CrossOrigin("*")
 public class TestController {
     @Autowired
     private TestService testService;
 
+    @PostMapping()
+    public ResponseEntity<?> createTest(@RequestBody TestDTO dto){
+        try {
+            return new ResponseEntity<>(testService.createTest(dto), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 }
