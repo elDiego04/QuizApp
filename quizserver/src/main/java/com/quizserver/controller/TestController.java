2f7975cecab6f09623202561d6dd6e4e58d16c29
Diego Salazar <diego.salazarc4@gmail.com>
2025-02-13 00:14:51 -0500

    Add notation post in TestController of question entity and test it.

--- quizserver/src/main/java/com/quizserver/controller/TestController.java
+++ quizserver/src/main/java/com/quizserver/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.quizserver.controller;
 
+import com.quizserver.dto.QuestionDTO;
 import com.quizserver.dto.TestDTO;
 import com.quizserver.service.test.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,14 @@ public class TestController {
         try {
             return new ResponseEntity<>(testService.createTest(dto), HttpStatus.OK);
         } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+    @PostMapping("/question")
+    public ResponseEntity<?> addQuestionInTest(@RequestBody QuestionDTO dto){
+        try {
+            return new ResponseEntity<>(testService.addQuestionInTest(dto), HttpStatus.CREATED);
+        } catch (Exception e){
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
